27b6dd7902cf78e3bcfc5720e36b8497d3099aa3
Ythalo Rossy <ythalorossy@gmail.com>
2018-11-06 01:14:49 -0500

    Configure to deploy on a public test network (truffle)

--- truffle.js
+++ truffle.js
@@ -1,18 +1,25 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+require('dotenv').config();
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = process.env["MNEMONIC"];
+const tokenKey = process.env["RIKNKENY_API_KEY"];
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: "*",
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${tokenKey}`);
+      },
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000,
+    },
+  },
 };
